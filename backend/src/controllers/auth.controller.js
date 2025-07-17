ec47996cf49ae483deb15e8edeb7f4a1677bf230
Yash Tonapi <yasht@SteepGraph.com>
2025-07-17 16:35:19 +0530

    Socket.io : live message sharing. (Built on top of node.js and express.js)

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import User from "../models/user.model.js";
 import { generateToken } from "../lib/utils.js";
 import bcrypt from "bcryptjs"
 import cloudinary from "../lib/cloudinary.js"
+
 export const signup = async (req, res) => {
     const {fullName, email, password} = req.body
     try {
@@ -52,7 +53,7 @@ export const signup = async (req, res) => {
         console.log("Error in signup controller", error.message);
         res.status(500).json({message: "Internal Server Error"})
     }
-}   
+};   
 export const login = async (req, res) => {
      const { email, password}= req.body
     try {
@@ -79,7 +80,7 @@ export const login = async (req, res) => {
        console.log("Error in login controller", error.message);
        res.status(500).json({message: "Internal Server Error"})
     }
-}   
+};
 export const logout = (req, res) => {
     try{
         res.cookie("jwt", "", {
@@ -91,7 +92,6 @@ export const logout = (req, res) => {
         res.status(500).json({message: "Internal Server Error"})
     }
 };
-
 export const updateProfile = async (req, res) => {
     try{
         const {profilePic} = req.body;
@@ -110,7 +110,6 @@ export const updateProfile = async (req, res) => {
         res.status(500).json({message: "Internal Server Error"})
     }
 };
-
 export const checkAuth = (req,res) =>{
     try{
         res.status(200).json(req.user);
